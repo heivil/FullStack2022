205cfacd804c0877133a5e43bec006006ddc60d7
Ville Heikkinen <heivil88@gmail.com>
2022-10-03 15:30:23 +0300

    testi2

--- Testi.js
+++ Testi.js
@@ -1,7 +1,30 @@
-function myFunction(i) {
-    if(luku <= 100 ){
+const myFunction = (i) => {
+    if(i <= 100 ){
         return ""
     } else {
         return "syötit luvun joka on suurempi kuin 100"
     }
 }
+
+const viikonpäivä = (päiväNumero) => {
+    switch (päiväNumero){
+        case 1:
+            return "maanantai"
+        case 2:
+            return "tiistai"
+        case 3:
+            return "keskiviikko"
+        case 4:
+            return "torstai"
+        case 5:
+            return "perjantai"
+        case 6:
+            return "lauantai"
+        case 7:
+            return "sunnuntai"
+        default:
+            console.log("en tunnista päivää");
+    }
+}
+
+console.log(viikonpäivä(4))
