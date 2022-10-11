afbcc72d898daf93bf9168b591991bb74e278f92
Ville Heikkinen <heivil88@gmail.com>
2022-10-11 15:42:14 +0300

    pieni korjaus

--- tentti-sovellus/src/App.js
+++ tentti-sovellus/src/App.js
@@ -5,19 +5,22 @@ import React, {useState} from 'react';
 const App = () => {
   const[tenttiNumero, setTentti] = useState(0);
 
-  let yKysymys1 = {kysymys: "Kumpi ja Kampi tappeli. Kumpi voitti?", vastaukset: ["Kumpi", "Kampi", "Kumpikin"]}
-  let yKysymys2 = {kysymys: "Kumpi painaa enemmän, kilo höyheniä vai kilo kiviä?", vastaukset: ["Kivet", "Höyhenet", "Painavat saman verran"] }
-  let bKysymys1 = {kysymys: "Minkä väristä on virtahevon hiki?", vastaukset: ["Sinistä", "Punaista", "Kirkasta"] }
-  let bKysymys2 = {kysymys: "Montako vähintään 500 neliömetrin kokoista järveä on Suomessa?", vastaukset: ["n. 57 000", "n. 168 000", "en ole laskenut"]}
+  let kysymys1 = {kysymys: "Kumpi ja Kampi tappeli. Kumpi voitti?", vastaukset: ["Kumpi", "Kampi", "Kumpikin"]}
+  let kysymys2 = {kysymys: "Kumpi painaa enemmän, kilo höyheniä vai kilo kiviä?", vastaukset: ["Kivet", "Höyhenet", "Painavat saman verran"] }
+  let kysymys3 = {kysymys: "Montako vähintään 500 neliömetrin kokoista järveä on Suomessa?", vastaukset: ["n. 57 000", "n. 168 000", "en ole laskenut"]}
+  let kysymys4 = {kysymys: "Minkä väristä on virtahevon hiki?", vastaukset: ["Sinistä", "Punaista", "Kirkasta"] }
+  let kysymys5 = {kysymys: "Paljonko on 100 + 100", vastaukset: ["200", "500", "69 höhö"] }
+  let kysymys6 = {kysymys: "Kumpi tuli ensin, muna vai kana?", vastaukset: ["Kana", "Muna", "Yhtäaikaa"] }
+  
 
-  let tentti1 = {nimi:"Yleistentti",
-                kysymystenMäärä:12,
-                kysymykset:[yKysymys1, yKysymys2]
+  let tentti1 = {nimi:"Tentti 1",
+                kysymystenMäärä:3,
+                kysymykset:[kysymys1, kysymys2, kysymys3]
                 }
 
-  let tentti2 = {nimi:"Biologian tentti",
-                kysymystenMäärä:10,
-                kysymykset:[bKysymys1, bKysymys2]
+  let tentti2 = {nimi:"Tentti 2",
+                kysymystenMäärä:3,
+                kysymykset:[kysymys4, kysymys5, kysymys6]
                 }
 
   let tentit = [tentti1, tentti2]
