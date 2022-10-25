b8ddc66d5e6ce74fca106ba4b35ffbe215baa745
Ville Heikkinen <heivil88@gmail.com>
2022-10-25 17:48:05 +0300

    tyylittelyä, vielä tosin löytyy bugi

--- react-harjoitus/src/App.js
+++ react-harjoitus/src/App.js
@@ -1,44 +1,71 @@
 import './App.css';
 import { useState, useReducer, useEffect } from "react"
 import axios from 'axios'
+import chuck from './chuck.jpg'
 
 function ChuckNorrisFaktat() {
 
-  const [appData, dispatch] = useReducer(reducer, { fakta: "",noudetaanFaktaa:false, faktanNoutoEpäonnistui:false, faktaLista: [], aikaVäli: 5, failLaskuri: 0});
+  const [appData, dispatch] = useReducer(reducer, { fakta: "",noudetaanFaktaa:false, 
+  faktanNoutoEpäonnistui:false, faktaLista: [], aikaVäli: 5, failLaskuri: 0, näytetyt: [], toistuvuusLaskuri: 0});
   const [ajastin, setAjastin] = useState();
 
-  //ei päivitä appdata.aikaväliä 
-  function ajoitettuFaktanHaku(aika){
+  //VANHA. Ei päivitä staten muuttujia funktion sisällä
+  /* function ajoitettuFaktanHaku(aika){
     clearTimeout(ajastin)
     haeFakta()
     //console.log(appData.aikaVäli)
     setAjastin(setTimeout(function(){ ajoitettuFaktanHaku(appData.aikaVäli) }, aika * 1000));
+  } */
+
+  function ajoitettuFaktanHaku(aika){
+    clearTimeout(ajastin)
+    //console.log(appData.aikaVäli)
+    setAjastin(setTimeout(function(){ haeFakta() }, aika * 1000));
   }
 
   function lopetaAjastin(){
+    console.log("ajastin lopetetaan")
     clearTimeout(ajastin)
   }
 
   function reducer(state, action) {
     switch (action.type) {
       case 'FAKTAN_NOUTO_ALOITETTU':
+
         console.log("Faktan nouto aloitettu")
         return { ...state, noudetaanFaktaa:true}
+
       case 'FAKTA_NOUDETTU':
+
         console.log("Fakta noudettu")
         const appDataKopio = JSON.parse(JSON.stringify(state))
         if(!state.faktaLista.includes(action.payload) && action.payload !== ""){
           appDataKopio.faktaLista.push(action.payload)
         }
-        return { ...state, fakta: action.payload, faktanNoutoEpäonnistui:false, noudetaanFaktaa:false, faktaLista: appDataKopio.faktaLista }
+        appDataKopio.näytetyt.push(action.payload)
+        return { ...state, fakta: action.payload, faktanNoutoEpäonnistui:false, 
+          noudetaanFaktaa: false, faktaLista: appDataKopio.faktaLista, näytetyt:appDataKopio.näytetyt }
+
+      case 'FAKTA_NÄYTETTY_JO':
+
+        const näytöt = state.toistuvuusLaskuri +1
+        console.log('Fakta on jo näytetty.', näytöt)
+        return {...state, toistuvuusLaskuri: näytöt}
+
       case 'FAKTAN_NOUTO_EPÄONNISTUI':
+
         console.log("Faktan nouto epäonnistui")
         const failLaskuriKopio = state.failLaskuri + 1;
-        return { ...state, faktanNoutoEpäonnistui:true, noudetaanFaktaa:false, failLaskuri: failLaskuriKopio}
+        return { ...state, faktanNoutoEpäonnistui: true, noudetaanFaktaa: false, failLaskuri: failLaskuriKopio}
+
       case 'AIKAVÄLI_MUUTETTU':
+
         return{...state, aikaVäli: action.payload}
+
       case 'DATA_ALUSTETTU':
+
         return{...state, faktaLista: action.payload}
+
       default:
         throw new Error("Reducer meni defaultiin.");
     }
@@ -54,45 +81,67 @@ function ChuckNorrisFaktat() {
       dispatch({type: 'DATA_ALUSTETTU', payload: JSON.parse(ladattuData)})
     }
     clearTimeout(ajastin)
-    setAjastin(setTimeout(function(){ ajoitettuFaktanHaku(appData.aikaVäli) }, appData.aikaVäli * 1000));
+    ajoitettuFaktanHaku(appData.aikaVäli)
   }, []);
 
   useEffect(() => {
     localStorage.setItem('chuckNorrisData', JSON.stringify(appData.faktaLista));
   }, [appData.faktaLista])
 
+  useEffect(()=>{
+    ajoitettuFaktanHaku(appData.aikaVäli)
+  }, [appData.fakta])
+
   async function haeFakta() {
-    try{
-      dispatch({type: 'FAKTAN_NOUTO_ALOITETTU'})
-      let result;
-      if(appData.failLaskuri < 4){
-        result = await axios('https://api.chucknorris.io/jokes/random'); 
-        if(!appData.faktanNoutoEpäonnistui)dispatch({type: 'FAKTA_NOUDETTU', payload: result.data.value})
-      }else if (appData.failLaskuri < 7){
-        result = appData.faktaLista[Math.floor(Math.random() * appData.faktaLista.length)]
-        console.log(appData.faktaLista.length)
-        if(!appData.faktanNoutoEpäonnistui)dispatch({type: 'FAKTA_NOUDETTU', payload: result})
+      try{
+        console.log("faktoja:", appData.faktaLista.length, "toistoja", appData.toistuvuusLaskuri)
+        if(appData.toistuvuusLaskuri < 4){
+          dispatch({type: 'FAKTAN_NOUTO_ALOITETTU'})
+          let result;
+          if(appData.failLaskuri < 0){
+            result = await axios('https://api.chucknorris.io/jokes/random'); 
+            if(!appData.faktanNoutoEpäonnistui){
+              if(appData.näytetyt.includes(result)){
+                dispatch({type:'FAKTA_NÄYTETTY_JO'})
+                ajoitettuFaktanHaku(appData.aikaVäli)
+              }else{
+                dispatch({type: 'FAKTA_NOUDETTU', payload: result.data.value})
+              }
+            }
+          }else if (appData.failLaskuri < 7){
+            result = appData.faktaLista[Math.floor(Math.random() * appData.faktaLista.length)]
+            if(!appData.faktanNoutoEpäonnistui){
+              if(appData.näytetyt.includes(result)){
+                dispatch({type:'FAKTA_NÄYTETTY_JO'})
+                ajoitettuFaktanHaku(appData.aikaVäli)
+              }else{
+                dispatch({type: 'FAKTA_NOUDETTU', payload: result})
+              }
+            }
+          }      
+        }
+      } catch (error){
+        console.log("Erroria pukkaa: ", error)
+        dispatch({type: 'FAKTAN_NOUTO_EPÄONNISTUI'})
       }
-      
-      
-    } catch (error){
-      console.log("Erroria pukkaa: ", error)
-      dispatch({type: 'FAKTAN_NOUTO_EPÄONNISTUI'})
-    }
   }
 
   return (
-    <div>
+    <div className='Main-content'>
+      <img className='Chuck' src={chuck} alt='Chuck' onClick={()=>{ appData.toistuvuusLaskuri<4 && ajoitettuFaktanHaku(appData.aikaVäli)}}/>
       <div>{appData.fakta}</div>
-      <input type="number" value = {appData.aikaVäli} onChange={(event)=>{dispatch({type: 'AIKAVÄLI_MUUTETTU', payload: event.target.value})}}/>
 
-      <button onClick={()=>{ ajoitettuFaktanHaku(appData.aikaVäli)}}>Hae vitsi</button>
+      {/* <button className='Nappi' onClick={()=>{ appData.toistuvuusLaskuri<4 && ajoitettuFaktanHaku(appData.aikaVäli)}}>Hae vitsi</button> */}
 
-      <button onClick={()=>{lopetaAjastin()}}> Lopeta generointi</button>
+      <button className='Nappi' onClick={()=>{lopetaAjastin()}}> Lopeta generointi</button>
+
+      <p> Päivitä <input type="number" value = {appData.aikaVäli} onChange={(event)=>{dispatch({type: 'AIKAVÄLI_MUUTETTU', payload: event.target.value})}}/> sekunnin välein</p>
 
       <p>{appData.noudetaanFaktaa && "Noudetaan faktaa Chuck Norriksesta!"}</p>
 
       <p>{appData.faktanNoutoEpäonnistui && "Faktan nouto epäonnistui"}</p>
+
+      <p>{appData.toistuvuusLaskuri >=4 && "Nyt on vitsit vähissä"}</p>
     </div>
   );
 }
