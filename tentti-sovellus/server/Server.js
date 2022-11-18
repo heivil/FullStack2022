5e252d0eaf3007c8ba76c0eac590d4689f6ac305
Ville Heikkinen <heivil88@gmail.com>
2022-11-18 13:21:46 +0200

    ssl salaus ja sähköpostin lähetys serveriltä

--- tentti-sovellus/server/Server.js
+++ tentti-sovellus/server/Server.js
@@ -1,21 +1,62 @@
-//const fs = require('fs');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
+const fs = require('fs');
 //const { findSourceMap } = require('module');
 //const bodyparser = require('body-parser');
 //const { getByTestId } = require('@testing-library/react');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8080;
+//const port = 8080;
 const login = require("./Komennot/Login");
 const tentit = require("./Komennot/Tentit")
 const kysymykset = require("./Komennot/Kysymykset")
 const vastaukset = require("./Komennot/Vastaukset")
 const verifoi = require("./Verifoi")
+const https = require("https");
+const nodemailer = require('nodemailer');
 
 app.use(cors());
 app.use(express.json());
 /* app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json()) */
+
+
+https
+.createServer(
+  // Provide the private and public key to the server by reading each
+  // file's content with the readFileSync() method.
+  {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+  },
+  app
+)
+.listen(4000, () => {
+  console.log("serever is runing at port 4000");
+});
+
+/* var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'heivil88@gmail.com',
+    pass: 'ulsewkrriugsjtnh'
+  }
+});
+
+var mailOptions = {
+  from: 'heivil88@gmail.com',
+  to: 'heivil88@gmail.com, juvuorin@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'asd, Ville Heikkinen'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});  */
 
 app.post('/rekisteroi/tunnus/:tunnus/salasana/:salasana/tentti_id/:tentti_id/onko_admin/:onko_admin', (req, res) => {
   login.rekisteröi(req, res)
@@ -25,7 +66,7 @@ app.get('/kirjaudu/tunnus/:tunnus/salasana/:salasana', (req, res) => {
   login.kirjaudu(req, res)
 })
 
-//app.use(verifoi.verifyToken)
+app.use(verifoi.verifyToken)
 
 app.get('/tentti/id/:tentti_id', (req, res,) => { 
   tentit.lataaTenttiIdllä(req, res)
@@ -72,14 +113,6 @@ app.delete('/poistaVastaus/id/:id', (req, res) => {
 })
 
 
-app.listen(port, () => {
+/* app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
-
-/* app.use(komennot.varasLähtö)
-
-app.get("/testi", komennot.yksi , async (req, res) => {
-  komennot.kaksi(req, res)
-  console.log("kolme")
-  res.send("päästiin perille")
-}); */
+}) */
