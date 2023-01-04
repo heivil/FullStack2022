f886c7445c17d9455975e1a56d05c3ac2cd75a81
Ville Heikkinen <heivil88@gmail.com>
2023-01-04 15:21:18 +0200

    loput toiminnot chatpalvelimeen

--- tentti-sovellus/server/SocketClient.js
+++ tentti-sovellus/server/SocketClient.js
@@ -3,28 +3,29 @@ var readline = require('readline');
 
 var client = new net.Socket();
 var rl = null;
+
 client.connect(1337, '127.0.0.1', () => {
     console.log('Connected');
+    console.log('Lähetä yksityisviesti kirjoittamalla viestin alkuun "*tunnus" esim: *erkki viesti');
 
     rl = readline.createInterface(process.stdin, process.stdout);
 
     rl.on('line', (teksti) => {
-        client.write(teksti)
+        client.write(teksti);
     });
     rl.setPrompt(`>`);
 
 });
 
 client.on('error', (err) => {
-  console.log('Error tuli' + err);
-
+    console.log('Error tuli' + err);
 });
 
 client.on('data', (data) => {
 
     console.log('--' + data);
 
-    rl.prompt()
+    rl.prompt();
 });
 
 client.on('close', () => {
--- tentti-sovellus/server/SocketServer.js
+++ tentti-sovellus/server/SocketServer.js
@@ -5,6 +5,7 @@ var serverinSoketti = null;
 
 var chättääjät = []
 const kiroSanat = ["vittu", "perkele", "saatana", "kyrpä", "homo", "huora", "paska", "kusi", "jumalauta", "helvetti", "johanna tukiainen" ]
+const ulostautumisAika = 120000
 
 var server = net.createServer((socket) => {
 	
@@ -14,16 +15,45 @@ var server = net.createServer((socket) => {
 
 	const heitäUlos = () => {
 		clearTimeout(socket.ajastin);
-		console.log("heitetään ulos")
+		console.log(socket.tunnus + " heitetään ulos")
     socket.destroy()
 	}
-	socket.ajastin = setTimeout(heitäUlos, 30000);
+
+	const nollaaPuheLaskuri = () => {
+		socket.puheLaskuri = 0;
+	}
+
+	const puhuuLiikaa = () => {
+		clearTimeout(socket.puhuuLiikaaAjastin);
+		chättääjät.forEach((item)=>{
+			item.write(socket.tunnus + " heitetään ulos, koska hän puhuu liikaa!")
+		})
+		socket.destroy();
+	}
+
+
+	socket.puhuuLiikaaAjastin = setTimeout(nollaaPuheLaskuri, 1000);
+	socket.puheLaskuri = 0;
+
+	socket.ajastin = setTimeout(heitäUlos, ulostautumisAika);
+	
 	
 	socket.on('error', (err) => {
 		console.log("Socketissa error: " + err)
 	})
 
 	socket.on('data', (data) => {
+
+		//katsotaan puhuuko joku liikaa
+		clearTimeout(socket.puhuuLiikaaAjastin);
+    socket.puhuuLiikaaAjastin = setTimeout(nollaaPuheLaskuri, 1000);
+    socket.puheLaskuri++;
+    if (socket.puheLaskuri >= 10) {
+        puhuuLiikaa();
+        return;
+    }
+
+		//kirosanafiltteri
 		for(i = 0; i < kiroSanat.length; i++){
 			if(data.toString().toLowerCase().includes(kiroSanat[i])){
 				console.log("Kiroilu ei ole sallittu")
@@ -32,27 +62,56 @@ var server = net.createServer((socket) => {
 				return
 			}
 		}
+
+		//aloitetaan ajastin, joka mittaa kuinka kauan käyttäjä on ollut hiljaa, uudelleen 
 		if(socket.ajastin) {
 			clearTimeout(socket.ajastin);
-			socket.ajastin = setTimeout(heitäUlos, 30000);
+			socket.ajastin = setTimeout(heitäUlos, ulostautumisAika);
 		}
-		chättääjät.forEach((item) => {
+
+		//salaisella stringillä saa potkuoideuden
+		if(data.toString() === "Anna minulle potkuoikeudet pliis!"){
+			socket.kickAuth = true
+			socket.write("Potkuoikeus myönnetty.")
+			return
+		}
+		
+		//yksityisviesti tai kickpyyntö
+		for(i = 0; i < chättääjät.length; i++){
+			if(socket.tunnus !== undefined && data.toString().startsWith("*" + chättääjät[i].tunnus)){
+				chättääjät[i].write(socket.tunnus + " lähetti yksityisviestin: " + data.toString().replace("*" + chättääjät[i].tunnus, ""))
+				return;
+			}	else if(socket.kickAuth === true && data.toString().startsWith("KICK: " + chättääjät[i].tunnus)){
+				console.log(chättääjät[i].tunnus + " heitetään ulos käyttäjän " + socket.tunnus + " pyynnöstä")
+				chättääjät.forEach((item)=>{
+					item.write(chättääjät[i].tunnus + " heitetään ulos käyttäjän " + socket.tunnus + " pyynnöstä")
+				})
+				chättääjät[i].destroy()
+				return;
+			}
+		}
+
+		//ensimmäinen palvelimelle tullut viesti asetetaan käyttäjätunnukseksi, muuten lähetetään viesti kaikille käyttäjille
+		for(i = 0; i < chättääjät.length; i++){
 			if(socket.tunnus === undefined){
 				socket.tunnus = data
 				console.log("tunnus:" + socket.tunnus)
-				item.write(socket.tunnus + " liittyi chattiin: ")
+				chättääjät.forEach((item)=>{
+					item.write(socket.tunnus + " liittyi chattiin.")
+				})
+				return
 			}else{ 
-				item.write(socket.tunnus + " sanoo: " + data)
+				chättääjät[i].write(socket.tunnus + " sanoo: " + data)
 			}
-		})
+		}
+
 	});
+
 
 	socket.on('close', () => {
 		console.log('Connection closed');
 	});
 
-	//socket.pipe(socket);
 });
-
 
 server.listen(1337, '127.0.0.1');
